33dcf4f96ebf374108527d0224d6d86f15677c57
ji-circle <sp202401jw@gmail.com>
2024-04-01 21:00:05 +0900

    인증번호 수정하기

--- app/src/main/java/com/example/spartateamproject/SignInActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignInActivity.kt
@@ -1,11 +1,23 @@
 package com.example.spartateamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+        var btn_join = findViewById<Button>(R.id.btn_signup)
+
+        btn_join.setOnClickListener {
+            val changeIntent = Intent(this,SignUpActivity::class.java)
+            startActivity(changeIntent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/spartateamproject/SignUpActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignUpActivity.kt
@@ -25,9 +25,15 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var et_userAuthent : EditText
 
     lateinit var tv_authent : TextView
+    lateinit var shownAuthent : String
 
     lateinit var btn_checkId : Button
+    lateinit var btn_createAuthent : Button
+    lateinit var btn_checkAuthent : Button
     lateinit var btn_join : Button
+
+    var isIdOk = false
+    var isAuthentOk = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,8 +48,11 @@ class SignUpActivity : AppCompatActivity() {
         et_userAuthent = findViewById<EditText>(R.id.et_authent)
 
         tv_authent = findViewById<TextView>(R.id.tv_authent)
+        shownAuthent = tv_authent.text.toString()
 
         btn_checkId = findViewById<Button>(R.id.btn_checkId)
+        btn_createAuthent = findViewById<Button>(R.id.btn_newAuthent)
+        btn_checkAuthent = findViewById<Button>(R.id.btn_checkAuthent)
         btn_join = findViewById<Button>(R.id.btn_join)
 
         var newName = et_userName.text.toString()
@@ -53,34 +62,92 @@ class SignUpActivity : AppCompatActivity() {
         var newEmail = et_userName.text.toString()
         var newAuthent = et_userName.text.toString()
 
-        var isIdOk = false
 
 
         btn_checkId.setOnClickListener {
-            if(idList.isNotEmpty()){
-                if(idList.contains(newId)){
-                    isIdOk=false
-                }else{
-                    isIdOk=true
-                }
+            checkId(newId)
+        }
+
+        shownAuthent = generateAuthent(6)
+        tv_authent.text = shownAuthent
+
+        btn_createAuthent.setOnClickListener {
+            shownAuthent = generateAuthent(6)
+            tv_authent.text = shownAuthent
+        }
+
+        btn_checkAuthent.setOnClickListener {
+            if(shownAuthent==newAuthent){
+                isAuthentOk = true
+                Toast.makeText(this,getString(R.string.authent_correct), Toast.LENGTH_SHORT).show()
+                btn_checkAuthent.text = getString(R.string.authent_correct)
+                //버튼 글자 바뀌게 했는데... 이게 작동할지는 모르겠어요
             }else{
-                isIdOk = true
-            }
-            if(isIdOk){
-                Toast.makeText(this,getText(R.string.available_id), Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this,getText(R.string.nonavailable_id), Toast.LENGTH_SHORT).show()
+                isAuthentOk = false
+                Toast.makeText(this,getString(R.string.authent_not_correct), Toast.LENGTH_SHORT).show()
             }
         }
 
+        btn_join.setOnClickListener {
+            checkAndReg(newName, newId, newPw, checkPw, newEmail, newAuthent)
+        }
 
 
     }
 
+    fun checkId(newId : String){
+        if(idList.isNotEmpty()){
+            if(idList.contains(newId)){
+                isIdOk=false
+            }else{
+                isIdOk=true
+            }
+        }else{
+            isIdOk = true
+        }
+        if(isIdOk){
+            Toast.makeText(this,getText(R.string.available_id), Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this,getText(R.string.nonavailable_id), Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun generateAuthent(length: Int): String{
+        val charList : List<Char> = ('a'..'z')+('A'..'Z')+('0'..'9')
+        return(1..length).map{charList.random()}.joinToString {""}
+    }
 
+    private fun checkAndReg(newName: String, newId: String, newPw: String, checkPw: String, newEmail: String, newAuthent: String) {
+        if(newName.isEmpty()||newId.isEmpty()||newPw.isEmpty()||checkPw.isEmpty()||newEmail.isEmpty()||newAuthent.isEmpty()){
+            Toast.makeText(this,getString(R.string.empty_in_et), Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(newPw!=checkPw){
+            Toast.makeText(this,getString(R.string.pw_not_same),Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(!isIdOk){
+            Toast.makeText(this,getString(R.string.id_not_available),Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(!newEmail.contains('@')){
+            Toast.makeText(this,getString(R.string.email_not_correct),Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(newAuthent!=shownAuthent){
+            Toast.makeText(this,getString(R.string.authent_not_correct),Toast.LENGTH_SHORT).show()
+            return
+        }
 
+        val objUserInfo: MutableMap<String, String> = mutableMapOf()
+        objUserInfo["name"]=newName
+        objUserInfo["id"]=newId
+        objUserInfo["pw"]=newPw
+        objUserInfo["email"]=newEmail
 
+        UserDataList.userDataList.add(objUserInfo)
+
+    }
 
 
 }
