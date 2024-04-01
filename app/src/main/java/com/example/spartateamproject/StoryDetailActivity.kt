b3d50efcdf093f9febe192df2ee4b7d2cb196d0a
HP <nsisn@naver.com>
2024-04-01 16:20:50 +0900

    init

---
+++ app/src/main/java/com/example/spartateamproject/StoryDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spartateamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_detail)
+    }
+}
