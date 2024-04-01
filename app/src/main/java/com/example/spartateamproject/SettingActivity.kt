48649d2451ae1fa32211c8cfa1da9f22e84e8aba
yeonsoo940630 <wjddustn26@gmail.com>
2024-04-01 17:34:19 +0900

    test

--- app/src/main/java/com/example/spartateamproject/SettingActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SettingActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) // 테스트 중
         setContentView(R.layout.activity_setting)
     }
 }
