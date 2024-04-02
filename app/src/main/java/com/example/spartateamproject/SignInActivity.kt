1152983d8d884c61af1c185764f590298c54398d
ji-circle <sp202401jw@gmail.com>
2024-04-02 20:35:06 +0900

    비밀번호 강도 수정필요

--- app/src/main/java/com/example/spartateamproject/SignInActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignInActivity.kt
@@ -1,23 +1,88 @@
 package com.example.spartateamproject
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var id : EditText
+    lateinit var password : EditText
+    lateinit var login : Button
+    lateinit var signup: Button
+
+    //이전에 회원가입한 적이 있는지 확인
+    var isFirst = true
+
+    //직전에 회원가입한 경우, 해당 계정 정보 가져옴
+    var prevInfo : ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        if(it.resultCode == Activity.RESULT_OK){
+            val prevId = it.data?.getStringExtra("userID")
+            val prevPw = it.data?.getStringExtra("userPW")
+            id.setText(prevId)
+            password.setText(prevPw)
+
+            isFirst = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-
-        var btn_join = findViewById<Button>(R.id.btn_signup)
-
-        btn_join.setOnClickListener {
-            val changeIntent = Intent(this,SignUpActivity::class.java)
-            startActivity(changeIntent)
+        init()
+        goToMain()
+        goToSignUp()
+        lifecycleScope.launch {
+            whenStarted {
+                while(true){
+                    isLoginAvailable()
+                    delay(500)
+                }
+            }
         }
     }
 
 
+
+    private fun init() {
+        id = findViewById(R.id.edit_sign_in_id)
+        password = findViewById(R.id.edit_sign_in_pwd)
+        login = findViewById(R.id.btn_login)
+        signup = findViewById(R.id.btn_signup)
+    }
+    private fun isLoginAvailable() {
+        var checkId = false
+        var checkPwd = false
+        if(id.text.length >= 5)
+            checkId = true
+        else
+            checkId = false
+        if(password.text.length >= 8)
+            checkPwd = true
+        else
+            checkPwd = false
+        login.isEnabled = checkId && checkPwd
+    }
+    private fun goToMain() {
+        login.setOnClickListener {
+            val intent = Intent(this@SignInActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun goToSignUp() {
+        signup.setOnClickListener {
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            prevInfo.launch(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/spartateamproject/SignUpActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignUpActivity.kt
@@ -1,36 +1,39 @@
 package com.example.spartateamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 //data class 새로 만들까요? 아니면 그냥 제가 원래 했던대로 할까요...?
-object UserDataList{
+object UserDataList {
     var userDataList = mutableListOf<Map<String, String>>()
 }
 
 class SignUpActivity : AppCompatActivity() {
 
     //아이디 중복체크 위한 리스트
-    val idList = UserDataList.userDataList.flatMap { it["id"]?.split(",")?: emptyList() }
+    val idList = UserDataList.userDataList.flatMap { it["id"]?.split(",") ?: emptyList() }
 
-    lateinit var et_userName : EditText
-    lateinit var et_userId : EditText
-    lateinit var et_userPw : EditText
-    lateinit var et_checkPw : EditText
-    lateinit var et_userEmail : EditText
-    lateinit var et_userAuthent : EditText
+    lateinit var et_userName: EditText
+    lateinit var et_userId: EditText
+    lateinit var et_userPw: EditText
+    lateinit var et_checkPw: EditText
+    lateinit var et_userEmail: EditText
+    lateinit var et_userAuthent: EditText
 
-    lateinit var tv_authent : TextView
-    lateinit var shownAuthent : String
+    lateinit var tv_authent: TextView
+    var generatedAuthent = ""
 
-    lateinit var btn_checkId : Button
-    lateinit var btn_createAuthent : Button
-    lateinit var btn_checkAuthent : Button
-    lateinit var btn_join : Button
+    lateinit var btn_checkId: Button
+    lateinit var btn_createAuthent: Button
+    lateinit var btn_checkAuthent: Button
+    lateinit var btn_join: Button
 
     var isIdOk = false
     var isAuthentOk = false
@@ -40,6 +43,8 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+//        Toast.makeText(this,"$idList",Toast.LENGTH_SHORT).show()
+
         et_userName = findViewById<EditText>(R.id.et_name)
         et_userId = findViewById<EditText>(R.id.et_id)
         et_userPw = findViewById<EditText>(R.id.et_pw)
@@ -48,104 +53,188 @@ class SignUpActivity : AppCompatActivity() {
         et_userAuthent = findViewById<EditText>(R.id.et_authent)
 
         tv_authent = findViewById<TextView>(R.id.tv_authent)
-        shownAuthent = tv_authent.text.toString()
 
         btn_checkId = findViewById<Button>(R.id.btn_checkId)
         btn_createAuthent = findViewById<Button>(R.id.btn_newAuthent)
         btn_checkAuthent = findViewById<Button>(R.id.btn_checkAuthent)
         btn_join = findViewById<Button>(R.id.btn_join)
 
-        var newName = et_userName.text.toString()
-        var newId = et_userName.text.toString()
-        var newPw = et_userName.text.toString()
-        var checkPw = et_userName.text.toString()
-        var newEmail = et_userName.text.toString()
-        var newAuthent = et_userName.text.toString()
-
+        lateinit var newName: String
+        lateinit var newId: String
+        lateinit var newPw: String
+        lateinit var checkPw: String
+        lateinit var newEmail: String
+        lateinit var newAuthent: String
 
 
         btn_checkId.setOnClickListener {
+            newId = et_userId.text.toString()
             checkId(newId)
         }
 
-        shownAuthent = generateAuthent(6)
-        tv_authent.text = shownAuthent
+        generateAuthent()
 
         btn_createAuthent.setOnClickListener {
-            shownAuthent = generateAuthent(6)
-            tv_authent.text = shownAuthent
+            generateAuthent()
+            btn_checkAuthent.text = getString(R.string.btn_checkAuthent)
+            btn_checkAuthent.visibility = View.VISIBLE
         }
 
         btn_checkAuthent.setOnClickListener {
-            if(shownAuthent==newAuthent){
+            newAuthent = et_userAuthent.text.toString()
+            if (generatedAuthent == newAuthent) {
                 isAuthentOk = true
-                Toast.makeText(this,getString(R.string.authent_correct), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.authent_correct), Toast.LENGTH_SHORT).show()
                 btn_checkAuthent.text = getString(R.string.authent_correct)
-                //버튼 글자 바뀌게 했는데... 이게 작동할지는 모르겠어요
-            }else{
+                btn_checkAuthent.visibility = View.GONE
+            } else {
                 isAuthentOk = false
-                Toast.makeText(this,getString(R.string.authent_not_correct), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.authent_not_correct), Toast.LENGTH_SHORT)
+                    .show()
+                generateAuthent()
             }
         }
 
         btn_join.setOnClickListener {
+            newName = et_userName.text.toString()
+            newId = et_userId.text.toString()
+            newPw = et_userPw.text.toString()
+            checkPw = et_checkPw.text.toString()
+            newEmail = et_userEmail.text.toString()
+            newAuthent = et_userAuthent.text.toString()
             checkAndReg(newName, newId, newPw, checkPw, newEmail, newAuthent)
+//            Toast.makeText(
+//                this,
+//                "${newName.isEmpty()} , ${newId.isEmpty()} , ${newPw.isEmpty()} , ${checkPw.isEmpty()} , ${newEmail.isEmpty()} , ${newAuthent.isEmpty()}",
+//                Toast.LENGTH_SHORT
+//            ).show()
+
         }
 
 
     }
 
-    fun checkId(newId : String){
-        if(idList.isNotEmpty()){
-            if(idList.contains(newId)){
-                isIdOk=false
-            }else{
-                isIdOk=true
+    fun checkId(newId: String) {
+        if (idList.isNotEmpty()) {
+            if (idList.contains(newId)) {
+                isIdOk = false
+                //             Toast.makeText(this,"11", Toast.LENGTH_SHORT).show()
+            } else {
+                isIdOk = true
+                //             Toast.makeText(this,"22", Toast.LENGTH_SHORT).show()
             }
-        }else{
+        } else {
             isIdOk = true
+            //         Toast.makeText(this,"33", Toast.LENGTH_SHORT).show()
         }
-        if(isIdOk){
-            Toast.makeText(this,getText(R.string.available_id), Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(this,getText(R.string.nonavailable_id), Toast.LENGTH_SHORT).show()
+        if (isIdOk && strongID(newId)) {
+            Toast.makeText(this, getText(R.string.available_id), Toast.LENGTH_SHORT).show()
+        } else {
+            if (!isIdOk) {
+                Toast.makeText(this, getText(R.string.nonavailable_id), Toast.LENGTH_SHORT).show()
+            }else if(!strongID(newId)) {
+                Toast.makeText(this, getText(R.string.id_not_strong), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun generateAuthent(length: Int): String{
-        val charList : List<Char> = ('a'..'z')+('A'..'Z')+('0'..'9')
-        return(1..length).map{charList.random()}.joinToString {""}
+
+    private fun generateAuthent() {
+        generatedAuthent = generateRandom()
+        //Log.d("SignUpActivity", "$generatedAuthent")
+        tv_authent.text = generatedAuthent
+        et_userAuthent.text.clear()
     }
 
-    private fun checkAndReg(newName: String, newId: String, newPw: String, checkPw: String, newEmail: String, newAuthent: String) {
-        if(newName.isEmpty()||newId.isEmpty()||newPw.isEmpty()||checkPw.isEmpty()||newEmail.isEmpty()||newAuthent.isEmpty()){
-            Toast.makeText(this,getString(R.string.empty_in_et), Toast.LENGTH_SHORT).show()
+    private fun generateRandom(): String {
+        val characters = ('a'..'z') + ('A'..'H')+('J'..'Z') + ('1'..'9')
+        val length = 6
+        return (1..length).map { characters.random() }.joinToString("")
+    }
+
+    private fun checkEmail(newEmail: String): Boolean {
+//        val emailSample = "[a-zA-Z0-9._-]+@[a-z]+||.+[a-z]+"
+        val emailSample = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+"
+
+        return if (!newEmail.matches(emailSample.toRegex())) {
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun strongID(newId: String): Boolean {
+        if (newId.length >= 5) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    //비밀번호 강도 부분... 수정중입니다.
+//    private fun checkPW(newPw: String):Boolean{
+//        val characters =
+//
+//    }
+
+    private fun checkAndReg(
+        newName: String,
+        newId: String,
+        newPw: String,
+        checkPw: String,
+        newEmail: String,
+        newAuthent: String
+    ) {
+        if (newName.isEmpty() || newId.isEmpty() || newPw.isEmpty() || checkPw.isEmpty() || newEmail.isEmpty() || newAuthent.isEmpty()) {
+            Toast.makeText(this, getString(R.string.empty_in_et), Toast.LENGTH_SHORT).show()
             return
         }
-        if(newPw!=checkPw){
-            Toast.makeText(this,getString(R.string.pw_not_same),Toast.LENGTH_SHORT).show()
+        if (!strongID(newId)) {
+            Toast.makeText(this, getString(R.string.id_not_strong), Toast.LENGTH_SHORT).show()
             return
         }
-        if(!isIdOk){
-            Toast.makeText(this,getString(R.string.id_not_available),Toast.LENGTH_SHORT).show()
+        if (newPw != checkPw) {
+            Toast.makeText(this, getString(R.string.pw_not_same), Toast.LENGTH_SHORT).show()
             return
         }
-        if(!newEmail.contains('@')){
-            Toast.makeText(this,getString(R.string.email_not_correct),Toast.LENGTH_SHORT).show()
+        //비밀번호 강도 부분 수정중입니다.
+//        if(){
+//
+//        }
+        if (!isIdOk) {
+            Toast.makeText(this, getString(R.string.id_not_available), Toast.LENGTH_SHORT)
+                .show()
             return
         }
-        if(newAuthent!=shownAuthent){
-            Toast.makeText(this,getString(R.string.authent_not_correct),Toast.LENGTH_SHORT).show()
+        if (!checkEmail(newEmail)) {
+            Toast.makeText(this, getString(R.string.email_not_correct), Toast.LENGTH_SHORT)
+                .show()
             return
+        }
+        if (newAuthent != generatedAuthent) {
+            Toast.makeText(this, getString(R.string.authent_not_correct), Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        if(!isAuthentOk){
+            Toast.makeText(this,getString(R.string.authent_check_required),Toast.LENGTH_SHORT).show()
         }
 
         val objUserInfo: MutableMap<String, String> = mutableMapOf()
-        objUserInfo["name"]=newName
-        objUserInfo["id"]=newId
-        objUserInfo["pw"]=newPw
-        objUserInfo["email"]=newEmail
+        objUserInfo["name"] = newName
+        objUserInfo["id"] = newId
+        objUserInfo["pw"] = newPw
+        objUserInfo["email"] = newEmail
 
         UserDataList.userDataList.add(objUserInfo)
+
+        val returnIntent = Intent(this, SignInActivity::class.java)
+        returnIntent.putExtra("userID", newId)
+        returnIntent.putExtra("userPW", newPw)
+        setResult(RESULT_OK, returnIntent)
+
+        if (!isFinishing) finish()
+
 
     }
 
