90c8441c247cd1edb36c66300ef0c10155aa712a
HP <nsisn@naver.com>
2024-04-02 18:46:38 +0900

    2024-04-02-commit

--- app/src/main/java/com/example/spartateamproject/MainActivity.kt
+++ app/src/main/java/com/example/spartateamproject/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.spartateamproject
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
+import com.google.android.material.card.MaterialCardView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+val data = mutableListOf<String>(
+)
 class MainActivity : AppCompatActivity() {
+    lateinit var post : LinearLayout
+    var iconList = mutableListOf<MaterialCardView>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+
+        data.forEach {
+            val item = LayoutInflater.from(this@MainActivity).inflate(R.layout.item_post, null)
+            val itemText = item.findViewById<TextView>(R.id.tx_postText)
+            itemText.text = it
+            post.addView(item)
+        }
+
     }
+    private fun init() {
+        post = findViewById(R.id.linear_mainPost)
+        var num: Int = 9
+        for(i in 0..8){
+            data.add("TIL 작성 ${i+1}")
+        }
+        iconList.add(findViewById(R.id.card_main_member1))
+        iconList.add(findViewById(R.id.card_main_member2))
+        iconList.add(findViewById(R.id.card_main_member3))
+        iconList.add(findViewById(R.id.card_main_member4))
+        for(i in 0..iconList.size-1){
+            // 클래스가 없으므로 인텐트 이동만
+            iconList[i].setOnClickListener {
+                iconList[i].setStrokeColor(Color.rgb(0,0,0))
+                val intent = Intent(this@MainActivity, StoryDetailActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/spartateamproject/SignInActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignInActivity.kt
@@ -1,11 +1,67 @@
 package com.example.spartateamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var id : EditText
+    lateinit var password : EditText
+    lateinit var login : Button
+    lateinit var signup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        init()
+        goToMain()
+        goToSignUp()
+        lifecycleScope.launch {
+            whenStarted {
+                while(true){
+                    isLoginAvailable()
+                    delay(500)
+                }
+            }
+        }
+    }
+
+
+
+    private fun init() {
+        id = findViewById(R.id.edit_sign_in_id)
+        password = findViewById(R.id.edit_sign_in_pwd)
+        login = findViewById(R.id.btn_login)
+        signup = findViewById(R.id.btn_signup)
+    }
+    private fun isLoginAvailable() {
+        var checkId = false
+        var checkPwd = false
+        if(id.text.length >= 5)
+            checkId = true
+        else
+            checkId = false
+        if(password.text.length >= 8)
+            checkPwd = true
+        else
+            checkPwd = false
+        login.isEnabled = checkId && checkPwd
+    }
+    private fun goToMain() {
+        login.setOnClickListener {
+            val intent = Intent(this@SignInActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun goToSignUp() {
+        signup.setOnClickListener {
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
