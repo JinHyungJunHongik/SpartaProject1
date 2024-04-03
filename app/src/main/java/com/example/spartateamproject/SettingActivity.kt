9723d2e8ae461d492cbe0a1f31990413facc4bf2
yeonsoo940630 <wjddustn26@gmail.com>
2024-04-03 16:17:18 +0900

    test

--- app/src/main/java/com/example/spartateamproject/SettingActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SettingActivity.kt
@@ -1,9 +1,11 @@
 package com.example.spartateamproject
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 
 class SettingActivity : AppCompatActivity() {
 
@@ -11,10 +13,31 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        myPageBack()
+        editProfile()
+
+
+    }
+
+    private fun myPageBack() { // 뒤로가기
         val myPageBackButton = findViewById<ImageButton>(R.id.imgBtn_myPageBack)
 
         myPageBackButton.setOnClickListener {
             finish()
         }
     }
+
+    private fun editProfile() { // 프로필 편집으로 이동
+        val editProfile = findViewById<TextView>(R.id.tv_editProfile)
+
+        editProfile.setOnClickListener {
+
+        }
+    }
+
+
+
+
+
+
 }
