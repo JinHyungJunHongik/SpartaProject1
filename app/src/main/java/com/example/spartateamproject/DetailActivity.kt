12e2ad3458c6964b0f084e95d7ce6c6bbade1968
cococo35 <taejun0513@gmail.com>
2024-04-03 11:50:41 +0900

    email 링크 기능 추가

--- app/src/main/java/com/example/spartateamproject/DetailActivity.kt
+++ app/src/main/java/com/example/spartateamproject/DetailActivity.kt
@@ -2,10 +2,16 @@ package com.example.spartateamproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        // 기본 이메일 앱 연결
+        val email_link = findViewById<TextView>(R.id.tx_detail_email)
+        email_link.movementMethod = LinkMovementMethod.getInstance()
     }
 }
--- app/src/main/java/com/example/spartateamproject/SignInActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignInActivity.kt
@@ -1,53 +1,42 @@
 package com.example.spartateamproject
 
-<<<<<<< HEAD
 import android.app.Activity
-=======
->>>>>>> 90c8441c247cd1edb36c66300ef0c10155aa712a
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-<<<<<<< HEAD
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-=======
->>>>>>> 90c8441c247cd1edb36c66300ef0c10155aa712a
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.whenStarted
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-<<<<<<< HEAD
 
-=======
->>>>>>> 90c8441c247cd1edb36c66300ef0c10155aa712a
 
 class SignInActivity : AppCompatActivity() {
-    lateinit var id : EditText
-    lateinit var password : EditText
-    lateinit var login : Button
+    lateinit var id: EditText
+    lateinit var password: EditText
+    lateinit var login: Button
     lateinit var signup: Button
-<<<<<<< HEAD
 
     //이전에 회원가입한 적이 있는지 확인
     var isFirst = true
 
     //직전에 회원가입한 경우, 해당 계정 정보 가져옴
-    var prevInfo : ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    var prevInfo: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-        if(it.resultCode == Activity.RESULT_OK){
-            val prevId = it.data?.getStringExtra("userID")
-            val prevPw = it.data?.getStringExtra("userPW")
-            id.setText(prevId)
-            password.setText(prevPw)
+            if (it.resultCode == Activity.RESULT_OK) {
+                val prevId = it.data?.getStringExtra("userID")
+                val prevPw = it.data?.getStringExtra("userPW")
+                id.setText(prevId)
+                password.setText(prevPw)
 
-            isFirst = false
+                isFirst = false
+            }
         }
-    }
 
-=======
->>>>>>> 90c8441c247cd1edb36c66300ef0c10155aa712a
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -56,16 +45,13 @@ class SignInActivity : AppCompatActivity() {
         goToSignUp()
         lifecycleScope.launch {
             whenStarted {
-                while(true){
+                while (true) {
                     isLoginAvailable()
                     delay(500)
                 }
             }
         }
-<<<<<<< HEAD
-=======
     }
-
 
 
     private fun init() {
@@ -74,64 +60,32 @@ class SignInActivity : AppCompatActivity() {
         login = findViewById(R.id.btn_login)
         signup = findViewById(R.id.btn_signup)
     }
+
     private fun isLoginAvailable() {
         var checkId = false
         var checkPwd = false
-        if(id.text.length >= 5)
+        if (id.text.length >= 5)
             checkId = true
         else
             checkId = false
-        if(password.text.length >= 8)
+        if (password.text.length >= 8)
             checkPwd = true
         else
             checkPwd = false
         login.isEnabled = checkId && checkPwd
     }
+
     private fun goToMain() {
         login.setOnClickListener {
             val intent = Intent(this@SignInActivity, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
     private fun goToSignUp() {
         signup.setOnClickListener {
             val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
             startActivity(intent)
-        }
->>>>>>> 90c8441c247cd1edb36c66300ef0c10155aa712a
-    }
-
-
-
-    private fun init() {
-        id = findViewById(R.id.edit_sign_in_id)
-        password = findViewById(R.id.edit_sign_in_pwd)
-        login = findViewById(R.id.btn_login)
-        signup = findViewById(R.id.btn_signup)
-    }
-    private fun isLoginAvailable() {
-        var checkId = false
-        var checkPwd = false
-        if(id.text.length >= 5)
-            checkId = true
-        else
-            checkId = false
-        if(password.text.length >= 8)
-            checkPwd = true
-        else
-            checkPwd = false
-        login.isEnabled = checkId && checkPwd
-    }
-    private fun goToMain() {
-        login.setOnClickListener {
-            val intent = Intent(this@SignInActivity, MainActivity::class.java)
-            startActivity(intent)
-        }
-    }
-    private fun goToSignUp() {
-        signup.setOnClickListener {
-            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-            prevInfo.launch(intent)
         }
     }
 }
