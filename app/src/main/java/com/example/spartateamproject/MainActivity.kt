822521e206f289a99528379385c11c0befd42db6
ji-circle <sp202401jw@gmail.com>
2024-04-03 21:26:16 +0900

    login부분까지 수정 완료

--- app/src/main/java/com/example/spartateamproject/MainActivity.kt
+++ app/src/main/java/com/example/spartateamproject/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.spartateamproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/spartateamproject/SignInActivity.kt
+++ app/src/main/java/com/example/spartateamproject/SignInActivity.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.accessibility.AccessibilityManager.TouchExplorationStateChangeListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.lifecycleScope
@@ -16,29 +18,30 @@ import kotlinx.coroutines.launch
 
 
 class SignInActivity : AppCompatActivity() {
-    lateinit var image : ImageView
-    lateinit var id : EditText
-    lateinit var password : EditText
-    lateinit var login : Button
+    lateinit var image: ImageView
+    lateinit var id: EditText
+    lateinit var password: EditText
+    lateinit var login: Button
     lateinit var signup: Button
 
     //이전에 회원가입한 적이 있는지 확인
     var isFirst = true
 
     //직전에 회원가입한 경우, 해당 계정 정보 가져옴
-    var prevInfo : ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    var prevInfo: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-        if(it.resultCode == Activity.RESULT_OK){
-            val prevImage = it.data?.getStringExtra("userImage")?.toInt()
-            val prevId = it.data?.getStringExtra("userID")
-            val prevPw = it.data?.getStringExtra("userPW")
-            image.setImageResource(prevImage!!)
-            id.setText(prevId)
-            password.setText(prevPw)
+            if (it.resultCode == Activity.RESULT_OK) {
+                val prevImage = it.data?.getStringExtra("userImage")?.toInt()
+                val prevId = it.data?.getStringExtra("userID")
+                val prevPw = it.data?.getStringExtra("userPW")
+                image.setImageResource(prevImage!!)
+                id.setText(prevId)
+                password.setText(prevPw)
 
-            isFirst = false
+                isFirst = false
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,14 +51,13 @@ class SignInActivity : AppCompatActivity() {
         goToSignUp()
         lifecycleScope.launch {
             whenStarted {
-                while(true){
+                while (true) {
                     isLoginAvailable()
                     delay(500)
                 }
             }
         }
     }
-
 
 
     private fun init() {
@@ -65,25 +67,60 @@ class SignInActivity : AppCompatActivity() {
         login = findViewById(R.id.btn_login)
         signup = findViewById(R.id.btn_signup)
     }
+
     private fun isLoginAvailable() {
         var checkId = false
         var checkPwd = false
-        if(id.text.length >= 5)
+        if (id.text.length >= 5)
             checkId = true
         else
             checkId = false
-        if(password.text.length >= 8)
+        if (password.text.length >= 8)
             checkPwd = true
         else
             checkPwd = false
         login.isEnabled = checkId && checkPwd
     }
+
     private fun goToMain() {
         login.setOnClickListener {
-            val intent = Intent(this@SignInActivity, MainActivity::class.java)
-            startActivity(intent)
+
+            if (id.text.isEmpty()) {
+                Toast.makeText(this, getString(R.string.et_no_id), Toast.LENGTH_SHORT).show()
+            }
+            if (password.text.isEmpty()) {
+                Toast.makeText(this, getString(R.string.et_no_pw), Toast.LENGTH_SHORT).show()
+            }
+
+            if (id.text.isNotEmpty() && password.text.isNotEmpty()) {
+                var idText = id.text.toString()
+                var pwText = password.text.toString()
+                var isMatched = 0
+
+                UserDataList.userDataList.forEach { user ->
+                    var objID = user["id"]
+                    var objPW = user["pw"]
+
+                    if (objID == idText) {
+                        if (objPW == pwText) {
+                            isMatched = 2
+                            Toast.makeText(this, getString(R.string.login_ok), Toast.LENGTH_SHORT).show()
+                            val loginIntent = Intent(this@SignInActivity, MainActivity::class.java)
+                            //id 넘기기
+                            loginIntent.putExtra("id", idText)
+                            startActivity(loginIntent)
+                        } else {
+                        }
+                    } else {
+                    }
+                }
+                if (isMatched == 0) {
+                    Toast.makeText(this, getString(R.string.et_check_idpw), Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
+
     private fun goToSignUp() {
         signup.setOnClickListener {
             val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
