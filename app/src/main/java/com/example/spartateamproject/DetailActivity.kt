4b35bc44e58e6a89e79dfc5865a1c4b4f9c742a4
cococo35 <taejun0513@gmail.com>
2024-04-05 00:16:01 +0900

    DetailActivity 마무리 작업 & string 작업

--- app/src/main/java/com/example/spartateamproject/DetailActivity.kt
+++ app/src/main/java/com/example/spartateamproject/DetailActivity.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.whenStarted
 import kotlinx.coroutines.launch
@@ -21,6 +23,13 @@ class DetailActivity : AppCompatActivity() {
     lateinit var post: TextView
     lateinit var data : Member
     lateinit var setting: ImageButton
+    lateinit var img: de.hdodenhof.circleimageview.CircleImageView
+    private lateinit var mbti: TextView
+    private lateinit var personality: TextView
+    private lateinit var tmi: TextView
+    private lateinit var morepost: TextView
+    private lateinit var url: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -33,17 +42,9 @@ class DetailActivity : AppCompatActivity() {
         }
         init()
         isSettingActivate()
-        // 기본 이메일 앱 연결
+
         val email_link = findViewById<TextView>(R.id.tx_detail_email)
         email_link.movementMethod = LinkMovementMethod.getInstance()
-
-        // 블로그 링크 연결
-        val blog_link = findViewById<TextView>(R.id.tx_detail_morepost)
-        blog_link.setOnClickListener {
-            val url = "https://nochfm0513.tistory.com/"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(intent)
-        }
 
         val btn_back = findViewById<ImageButton>(R.id.btn_detail_back)
         btn_back.setOnClickListener {
@@ -55,6 +56,11 @@ class DetailActivity : AppCompatActivity() {
         name = findViewById(R.id.tx_detail_name)
         email = findViewById(R.id.tx_detail_email)
         post = findViewById(R.id.tx_detail_post)
+        img = findViewById(R.id.img_detail_profile)
+        mbti = findViewById(R.id.tx_detail_MBTI)
+        personality = findViewById(R.id.tx_detail_personality)
+        tmi = findViewById(R.id.tx_detail_TMI)
+        morepost = findViewById(R.id.tx_detail_morepost)
         setting = findViewById(R.id.btn_detail_setting)
         setting.setOnClickListener {
             val intent = Intent(this, SettingActivity::class.java)
@@ -70,6 +76,52 @@ class DetailActivity : AppCompatActivity() {
         }
         else
             post.text = data.postList[2]._txt
+
+        if(data._name == "태준") {
+            mbti.text = getText(R.string.mbti1)
+            personality.text = getText(R.string.personality1)
+            tmi.text = getText(R.string.tmi1)
+            img.setImageResource(R.drawable.img_04)
+            morepost.setOnClickListener {
+                url = getText(R.string.link1).toString()
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+        if(data._name == "형준") {
+            mbti.text = getText(R.string.mbti2)
+            personality.text = getText(R.string.personality2)
+            tmi.text = getText(R.string.tmi2)
+            img.setImageResource(R.drawable.img_01)
+            morepost.setOnClickListener {
+                url = getText(R.string.link2).toString()
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+        if(data._name == "지원") {
+            mbti.text = getText(R.string.mbti3)
+            personality.text = getText(R.string.personality3)
+            tmi.text = getText(R.string.tmi3)
+            img.setImageResource(R.drawable.img_02)
+            morepost.setOnClickListener {
+                url = getText(R.string.link3).toString()
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+        if(data._name == "연수") {
+            mbti.text = getText(R.string.mbti4)
+            personality.text = getText(R.string.personality4)
+            tmi.text = getText(R.string.tmi4)
+            img.setImageResource(R.drawable.img_03)
+            morepost.setOnClickListener {
+                url = getText(R.string.link4).toString()
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+
     }
     private fun isSettingActivate(){
        if (data._id == currentLoginUser._id)
